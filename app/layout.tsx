ca3196ad2d9eedf72c22681c7309ebd34385989d
ShahAlom47 <sahalom4729@gmail.com>
2025-12-23 22:31:13 +0600

    added convex provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { ThemeProvider } from "@/providers/theme-provider"
-import ConvexClientProvider from "@/providers/convex-client-provider"
 import { Toaster } from "react-hot-toast"
+import ConvexClientProvider from "@/providers/ConvexClientProvider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -19,17 +18,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
+        {/* <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        >
-          {/* <ConvexClientProvider> */}
-            {children}
+        > */}
+              <ConvexClientProvider>{children}</ConvexClientProvider>
+     
             <Toaster />
           {/* </ConvexClientProvider> */}
-        </ThemeProvider>
+        {/* </ThemeProvider> */}
       </body>
     </html>
   )
