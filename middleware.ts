3508348fd3b04e600fe7dbc41188276c6364ff4d
ShahAlom47 <sahalom4729@gmail.com>
2026-01-08 21:41:35 +0600

    added  middleware

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const session = req.cookies.get("session");
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+}
